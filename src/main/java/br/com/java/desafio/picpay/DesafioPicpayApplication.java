73b53a599501636178553e93c8322d020460d88a
sergio-rocha1 <sergio.rocha.borges@gmail.com>
2024-07-23 14:46:04 -0300

    fix: Adicionado dotenv no projeto; Adicionado arquivo .env para armazenar o usuário e senha do banco de dados; Alterado properties do spring para buscar as variáveis de ambiente de usuário e senha;

--- src/main/java/br/com/java/desafio/picpay/DesafioPicpayApplication.java
+++ src/main/java/br/com/java/desafio/picpay/DesafioPicpayApplication.java
@@ -1,5 +1,6 @@
 package br.com.java.desafio.picpay;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -9,6 +10,14 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 public class DesafioPicpayApplication {
 
 	public static void main(String[] args) {
+		// Carregar o arquivo .env da pasta resources
+		Dotenv dotenv = Dotenv.configure()
+				.filename("db-acess.env")
+				.load();
+
+		System.setProperty("MYSQL_USER", dotenv.get("MYSQL_USER"));
+		System.setProperty("MYSQL_PASSWORD", dotenv.get("MYSQL_PASSWORD"));
+
 		SpringApplication.run(DesafioPicpayApplication.class, args);
 	}
 
